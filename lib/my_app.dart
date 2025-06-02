3a8a6b9e6cc20f9b24ea5d558af688b506438f5b
oliwierstepien <oliwier.stepien@gmail.com>
2025-06-02 14:16:51 +0200

    refactor

---
+++ lib/my_app.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:mealapp/common/bloc/button/button_state_cubit.dart';
+import 'package:mealapp/core/configs/theme/app_theme.dart';
+import 'package:mealapp/domain/meal/usecases/get_meal.dart';
+import 'package:mealapp/l10n/app_localizations.dart';
+import 'package:mealapp/l10n/l10n.dart';
+import 'package:mealapp/presentation/category_meals/bloc/categories_display_cubit.dart';
+import 'package:mealapp/presentation/meal_details/bloc/favorite_meals_cubit.dart';
+import 'package:mealapp/presentation/meal_details/bloc/meals_display_cubit.dart';
+import 'package:mealapp/presentation/meal_details/bloc/vegetarian_filter_cubit.dart';
+import 'package:mealapp/presentation/shopping_list/bloc/shopping_list_cubit.dart';
+import 'package:mealapp/presentation/splash/bloc/splash_cubit.dart';
+import 'package:mealapp/routes/go_router.dart';
+import 'package:mealapp/service_locator.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => SplashCubit()..appStarted()),
+        BlocProvider(create: (context) => ButtonStateCubit()),
+        BlocProvider(create: (context) => FavoriteMealsCubit()),
+        BlocProvider(create: (context) => ShoppingListCubit()),
+        BlocProvider(create: (context) => CategoriesDisplayCubit()..displayCategories()),
+        BlocProvider(create: (_) => VegetarianFilterCubit()),
+        BlocProvider(
+          create: (context) => MealsDisplayCubit(useCase: sl<GetMealUseCase>()),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: router,
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.appTheme,
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: L10n.locals,
+        locale: const Locale('pl'),
+      ),
+    );
+  }
+}
