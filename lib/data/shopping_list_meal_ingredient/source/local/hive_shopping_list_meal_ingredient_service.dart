e2233a023ec985c50064a4113b9ad9433e0f9034
oliwierstepien <oliwier.stepien@gmail.com>
2025-07-31 12:20:23 +0200

    meal ingredient refactor

--- lib/data/shopping_list_meal_ingredient/source/local/hive_shopping_list_meal_ingredient_service.dart
+++ lib/data/shopping_list_meal_ingredient/source/local/hive_shopping_list_meal_ingredient_service.dart
@@ -1,199 +1,213 @@
 import 'package:hive/hive.dart';
 import 'package:mealapp/common/helper/debug_log/debug_log.dart';
-import 'package:mealapp/data/meal/model/ingredient_model.dart';
-import 'package:mealapp/data/meal/model/meal_model.dart';
 import 'package:mealapp/data/shopping_list_meal_ingredient/model/shopping_list_meal_ingredient_model.dart';
 
+
 abstract class HiveShoppingListMealIngredientService {
-  Future<List<ShoppingListMealIngredientModel>>
-      getMealIngredientFromShoppingList();
-  Future<void> addMealIngredientToShoppingList(
-      ShoppingListMealIngredientModel item);
-  Future<void> removeMealIngredientFromShoppingList(
-    ShoppingListMealIngredientModel item, {
-    bool isOnline = false,
-  });
-  Future<List<ShoppingListMealIngredientModel>>
-      getUnsyncedShoppingListMealIngredient();
-  Future<void> markShoppingListMealIngredientAsSynced(
-      String mealId, String ingredientId);
-  Future<List<ShoppingListMealIngredientModel>>
-      getUnsyncedChangesForShoppingListMealIngredient();
-  Future<void> restoreMealIngredientToShoppingList(
-      MealModel meal, IngredientModel ingredient, int portionCount);
-  Future<void> clearSyncedDeletedItems();
+ Future<List<ShoppingListMealIngredientModel>>
+     getMealIngredientFromShoppingList();
+ Future<void> addMealIngredientToShoppingList(
+     ShoppingListMealIngredientModel item);
+ Future<void> removeMealIngredientFromShoppingList(
+   ShoppingListMealIngredientModel item, {
+   bool isOnline = false,
+ });
+ Future<List<ShoppingListMealIngredientModel>>
+     getUnsyncedShoppingListMealIngredient();
+ Future<void> markShoppingListMealIngredientAsSynced(
+     String mealId, String ingredientId);
+ Future<List<ShoppingListMealIngredientModel>>
+     getUnsyncedChangesForShoppingListMealIngredient();
+ Future<void> restoreMealIngredientToShoppingList(
+     ShoppingListMealIngredientModel item);
+ Future<void> clearSyncedDeletedItems();
 }
 
+
 class HiveShoppingListMealIngredientServiceImpl
-    implements HiveShoppingListMealIngredientService {
-  Box<ShoppingListMealIngredientModel> get _box =>
-      Hive.box<ShoppingListMealIngredientModel>('shoppingListMealIngredients');
+   implements HiveShoppingListMealIngredientService {
+ Box<ShoppingListMealIngredientModel> get _box =>
+     Hive.box<ShoppingListMealIngredientModel>('shoppingListMealIngredients');
 
-  @override
-  Future<List<ShoppingListMealIngredientModel>>
-      getMealIngredientFromShoppingList() async {
-    return _box.values.where((model) => !model.isDeleted).toList();
-  }
 
-  @override
-  Future<void> addMealIngredientToShoppingList(
-      ShoppingListMealIngredientModel item) async {
-    final key = '${item.meal.mealId}_${item.ingredient.ingredientId}';
+ String _key(ShoppingListMealIngredientModel item) =>
+     '${item.meal.mealId}_${item.ingredient.ingredientId}';
 
-    final model = _box.get(key);
 
-    await _box.put(
-      key,
-      item.copyWith(
-        isSynced: model?.isSynced ?? false,
-        isDeleted: false,
-      ),
-    );
+ @override
+ Future<List<ShoppingListMealIngredientModel>>
+     getMealIngredientFromShoppingList() async {
+   return _box.values.where((model) => !model.isDeleted).toList();
+ }
 
-    final allIngredients = await getMealIngredientFromShoppingList();
-    debugLog(
-        '✅ Dodano składnik: ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
-        name: 'HiveService');
-    debugLog('🛒 Liczba składników w shopping list: ${allIngredients.length}',
-        name: 'HiveService');
-    debugLog('📋 Składniki:', name: 'HiveService');
-    for (final item in allIngredients) {
-      debugLog(
-          ' - ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
-          name: 'HiveService');
-    }
-  }
 
-  @override
-  Future<void> removeMealIngredientFromShoppingList(
-      ShoppingListMealIngredientModel item,
-      {bool isOnline = false}) async {
-    final key = '${item.meal.mealId}_${item.ingredient.ingredientId}';
-    final model = _box.get(key);
+ @override
+ Future<void> addMealIngredientToShoppingList(
+     ShoppingListMealIngredientModel item) async {
+   final key = _key(item);
+   final model = _box.get(key);
 
-    if (model != null) {
-      await _box.put(
-        key,
-        model.copyWith(
-          isSynced: false,
-          isDeleted: true,
-        ),
-      );
-    }
 
-    final allIngredients = await getMealIngredientFromShoppingList();
-    debugLog(
-        '❌ Usunięto składnik: ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
-        name: 'HiveService');
-    debugLog('🛒 Liczba składników w shopping list: ${allIngredients.length}',
-        name: 'HiveService');
-    debugLog('📋 Składniki:', name: 'HiveService');
-    for (final item in allIngredients) {
-      debugLog(
-          ' - ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
-          name: 'HiveService');
-    }
-  }
+   await _box.put(
+     key,
+     item.copyWith(
+       isSynced: model?.isSynced ?? false,
+       isDeleted: false,
+     ),
+   );
 
-  @override
-  Future<List<ShoppingListMealIngredientModel>>
-      getUnsyncedShoppingListMealIngredient() async {
-    return _box.values
-        .where((model) => !model.isSynced && !model.isDeleted)
-        .toList();
-  }
 
-  @override
-  Future<List<ShoppingListMealIngredientModel>>
-      getUnsyncedChangesForShoppingListMealIngredient() async {
-    return _box.values.where((model) => !model.isSynced).toList();
-  }
+   final allIngredients = await getMealIngredientFromShoppingList();
+   debugLog(
+       '✅ Dodano składnik: ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
+       name: 'HiveService');
+   debugLog('🛒 Liczba składników w shopping list: ${allIngredients.length}',
+       name: 'HiveService');
+   debugLog('📋 Składniki:', name: 'HiveService');
+   for (final item in allIngredients) {
+     debugLog(
+         ' - ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
+         name: 'HiveService');
+   }
+ }
 
-  @override
-  Future<void> markShoppingListMealIngredientAsSynced(
-      String mealId, String ingredientId) async {
-    final key = '${mealId}_$ingredientId';
-    final model = _box.get(key);
 
-    if (model != null) {
-      await _box.put(
-        key,
-        ShoppingListMealIngredientModel(
-          meal: model.meal,
-          ingredient: model.ingredient,
-          portionCount: model.portionCount,
-          isSynced: true,
-          isDeleted: model.isDeleted,
-        ),
-      );
+ @override
+ Future<void> removeMealIngredientFromShoppingList(
+     ShoppingListMealIngredientModel item,
+     {bool isOnline = false}) async {
+   final key = '${item.meal.mealId}_${item.ingredient.ingredientId}';
+   final model = _box.get(key);
 
-      debugLog(
-          '✅ Zaznaczono jako zsynchronizowany: $ingredientId (z posiłku: $mealId)',
-          name: 'HiveService');
-    }
-  }
 
-  @override
-  Future<void> restoreMealIngredientToShoppingList(
-      MealModel meal, IngredientModel ingredient, int portionCount) async {
-    final key = '${meal.mealId}_${ingredient.ingredientId}';
-    final existingModel = _box.get(key);
+   if (model != null) {
+     await _box.put(
+       key,
+       model.copyWith(
+         isSynced: false,
+         isDeleted: true,
+       ),
+     );
+   }
 
-    final modelToSave = existingModel != null
-        ? ShoppingListMealIngredientModel(
-            meal: existingModel.meal,
-            ingredient: existingModel.ingredient,
-            portionCount: portionCount,
-            isSynced: existingModel.isSynced, // Zachowaj stan synchronizacji
-            isDeleted: false,
-          )
-        : ShoppingListMealIngredientModel(
-            meal: meal,
-            ingredient: ingredient,
-            portionCount: portionCount,
-            isSynced: false,
-            isDeleted: false,
-          );
 
-    await _box.put(key, modelToSave);
+   final allIngredients = await getMealIngredientFromShoppingList();
+   debugLog(
+       '❌ Usunięto składnik: ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
+       name: 'HiveService');
+   debugLog('🛒 Liczba składników w shopping list: ${allIngredients.length}',
+       name: 'HiveService');
+   debugLog('📋 Składniki:', name: 'HiveService');
+   for (final item in allIngredients) {
+     debugLog(
+         ' - ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
+         name: 'HiveService');
+   }
+ }
 
-    final allIngredients = await getMealIngredientFromShoppingList();
-    final allItemsInBox = _box.values.toList();
 
-    debugLog(
-        '♻️ Przywrócono składnik: ${ingredient.ingredientName} (z posiłku: ${meal.title})',
-        name: 'HiveService');
-    debugLog('🔑 Klucz: $key', name: 'HiveService');
-    debugLog('📦 Ilość wpisów w Hive: ${allItemsInBox.length}',
-        name: 'HiveService');
-    debugLog(
-        '🛒 Liczba aktywnych składników w shopping list: ${allIngredients.length}',
-        name: 'HiveService');
-    debugLog('📋 Aktywne składniki:', name: 'HiveService');
-    for (final item in allIngredients) {
-      debugLog(
-          ' - ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
-          name: 'HiveService');
-    }
-    debugLog('🗑️ Usunięte składniki:', name: 'HiveService');
-    for (final item in allItemsInBox.where((m) => m.isDeleted)) {
-      debugLog(
-          ' - ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
-          name: 'HiveService');
-    }
-  }
+ @override
+ Future<List<ShoppingListMealIngredientModel>>
+     getUnsyncedShoppingListMealIngredient() async {
+   return _box.values
+       .where((model) => !model.isSynced && !model.isDeleted)
+       .toList();
+ }
 
-  @override
-  Future<void> clearSyncedDeletedItems() async {
-    final keysToDelete = _box.keys.where((key) {
-      final item = _box.get(key);
-      return item != null && item.isDeleted && item.isSynced;
-    }).toList();
 
-    for (final key in keysToDelete) {
-      await _box.delete(key);
-      debugLog('🧹 Usunięto trwale zsynchronizowany składnik z kluczem: $key',
-          name: 'HiveService');
-    }
-  }
+ @override
+ Future<List<ShoppingListMealIngredientModel>>
+     getUnsyncedChangesForShoppingListMealIngredient() async {
+   return _box.values.where((model) => !model.isSynced).toList();
+ }
+
+
+ @override
+ Future<void> markShoppingListMealIngredientAsSynced(
+     String mealId, String ingredientId) async {
+   final key = '${mealId}_$ingredientId';
+   final model = _box.get(key);
+
+
+ if (model != null) {
+   await _box.put(
+     key,
+     model.copyWith(isSynced: true),
+   ); 
+
+
+     debugLog(
+         '✅ Zaznaczono jako zsynchronizowany: $ingredientId (z posiłku: $mealId)',
+         name: 'HiveService');
+   }
+ }
+
+
+ @override
+ Future<void> restoreMealIngredientToShoppingList(
+     ShoppingListMealIngredientModel item) async {
+   final key = _key(item);
+   final existingModel = _box.get(key);
+
+
+   final modelToSave = existingModel != null
+       ? existingModel.copyWith(
+           isDeleted: false,
+           portionCount: item.portionCount,
+           // isSynced zostaje jak jest (po „delete” powinno być false)
+         )
+       : item.copyWith(
+           isDeleted: false,
+           isSynced: false, // nowy wpis => do synchronizacji
+         );
+
+
+   await _box.put(key, modelToSave);
+
+
+
+
+   // Logowanie stanu po przywróceniu składnika
+   final allIngredients = await getMealIngredientFromShoppingList();
+   final allItemsInBox = _box.values.toList();
+
+
+   debugLog(
+       '♻️ Przywrócono składnik: ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
+       name: 'HiveService');
+   debugLog('🔑 Klucz: $key', name: 'HiveService');
+   debugLog('📦 Ilość wpisów w Hive: ${allItemsInBox.length}',
+       name: 'HiveService');
+   debugLog(
+       '🛒 Liczba aktywnych składników w shopping list: ${allIngredients.length}',
+       name: 'HiveService');
+   debugLog('📋 Aktywne składniki:', name: 'HiveService');
+   for (final item in allIngredients) {
+     debugLog(
+         ' - ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
+         name: 'HiveService');
+   }
+   debugLog('🗑️ Usunięte składniki:', name: 'HiveService');
+   for (final item in allItemsInBox.where((m) => m.isDeleted)) {
+     debugLog(
+         ' - ${item.ingredient.ingredientName} (z posiłku: ${item.meal.title})',
+         name: 'HiveService');
+   }
+ }
+
+
+ @override
+ Future<void> clearSyncedDeletedItems() async {
+   final keysToDelete = _box.keys.where((key) {
+     final item = _box.get(key);
+     return item != null && item.isDeleted && item.isSynced;
+   }).toList();
+
+
+   for (final key in keysToDelete) {
+     await _box.delete(key);
+     debugLog('🧹 Usunięto trwale zsynchronizowany składnik z kluczem: $key',
+         name: 'HiveService');
+   }
+ }
 }
--- lib/presentation/meal_details/bloc/meals_display_cubit.dart
+++ lib/presentation/meal_details/bloc/meals_display_cubit.dart
@@ -3,21 +3,37 @@ import 'package:mealapp/common/helper/handle_firestore_operation/failure/failure
 import 'package:mealapp/presentation/meal_details/bloc/meals_display_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+
 class MealsDisplayCubit extends Cubit<MealsDisplayState> {
-  final UseCase useCase;
+ final UseCase useCase;
 
-  MealsDisplayCubit({required this.useCase}) : super(MealsInitialState());
 
-  Future<void> displayMeals({dynamic params}) async {
-    emit(MealsLoading());
-    final returnedData = await useCase.call(params: params);
-    returnedData.fold(
-      (error) => emit(MealsLoadingFailure(message: mapFailureToMessage(error))),
-      (data) => emit(MealsLoadingSuccess(meals: data)),
-    );
-  }
+ MealsDisplayCubit({required this.useCase}) : super(MealsInitialState());
 
-  void displayInitial() {
-    emit(MealsInitialState());
-  }
+
+ Future<void> displayMeals({dynamic params}) async {
+   if (isClosed) return;
+   emit(MealsLoading());
+
+
+   final returnedData = await useCase.call(params: params);
+
+
+   if (isClosed) return;
+   returnedData.fold(
+     (error) {
+       if (!isClosed) {
+         emit(MealsLoadingFailure(message: mapFailureToMessage(error)));
+       }
+     },
+     (data) {
+       if (!isClosed) emit(MealsLoadingSuccess(meals: data));
+     },
+   );
+ }
+
+
+ void displayInitial() {
+   emit(MealsInitialState());
+ }
 }
